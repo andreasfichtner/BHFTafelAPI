7dc4fa03d95eeaabc9ffe08a3fb5a5c21ef1d1aa
Andreas <andreas.fichtner@gmail.com>
2017-05-20 01:57:50 +0200

    Read correct values from html

--- src/main/kotlin/de/retterdesapok/bhftafel/NetworkClient.kt
+++ src/main/kotlin/de/retterdesapok/bhftafel/NetworkClient.kt
@@ -1,4 +1,5 @@
 package de.retterdesapok.bhftafel
+
 import org.jsoup.Jsoup
 import java.net.HttpURLConnection
 import java.net.URL
@@ -9,11 +10,54 @@ import java.net.URLDecoder
 
 
 fun main(args: Array<String>) {
-    val urlParameters = "input=Unterschlei%DFheim&inputRef=%23&date=Fr%2C+19.05.17&time=21%3A28&productsFilter=1111101000000000&REQTrain_name=&maxJourneys=10&start=Suchen&boardType=Abfahrt&ao=yes"
+    var station = "Ostbahnhof"
+    var date = "20.05.17"
+    var time = "01:24"
+    var productsFilter = "1111111111111111"
+
+    var html = getHtmlData(station, date, time, productsFilter)
+
+    var htmlDocument = Jsoup.parse(html);
+    val mainDiv = htmlDocument.select("div.clicktable")
+    val entries = mainDiv.select("div.sqdetailsDep.trow")
+
+    entries.forEach {
+        var line = it.select("a span").text().replace("\\s*", "")
+        var destination = it.childNode(1).outerHtml().substringAfter("&gt;&gt;").replace("\\s+,", "")
+
+        var scheduledTime = it.select("span.bold")[1].text().replace("\\s*", "")
+        var delay: Int? = null
+
+        if (it.select("span").size >= 3) {
+            delay = it.select("span")[2].text().replace("\\s+", "").toInt()
+        }
+
+
+        println("$line \t\t\t\t$destination\t\t\t\t$scheduledTime\t\t\t\t$delay")
+    }
+}
+
+fun getHtmlData(station: String,
+                date: String,
+                time: String,
+                productsFilter: String,
+                numberOfJourneys: Int = 10): String {
+
+    val urlParameters = "" +
+            "input=${station}&inputRef=%23" +
+            "&date=${date}" +
+            "&time=${time}" +
+            "&productsFilter=${productsFilter}" +
+            "&REQTrain_name=" +
+            "&maxJourneys=${numberOfJourneys}" +
+            "&start=Suchen" +
+            "&boardType=Abfahrt" +
+            "&ao=yes"
+
     val postData = urlParameters.toByteArray(StandardCharsets.UTF_8)
     val postDataLength = postData.size
-    val request = "https://mobile.bahn.de/bin/mobil/bhftafel.exe/dox?country=DEU&rt=1&use_realtime_filter=1&webview=&"
-    val url = URL(request)
+    val requestURL = "https://mobile.bahn.de/bin/mobil/bhftafel.exe/dox?country=DEU&rt=1&use_realtime_filter=1&webview=&"
+    val url = URL(requestURL)
     val conn = url.openConnection() as HttpURLConnection
     conn.setDoOutput(true)
     conn.setInstanceFollowRedirects(false)
@@ -28,32 +72,14 @@ fun main(args: Array<String>) {
     val reader = BufferedReader(InputStreamReader(conn.inputStream))
     val sb = StringBuilder()
 
-    var foundTable = false;
-
-    var currentResponseItem : ResponseItem? = null;
-    var lineWithinCurrentItem = 0;
-
     var line = reader.readLine()
-    var decodedLine : String?
 
     while (line != null) {
-        decodedLine = URLDecoder.decode(line, "UTF-8")
-        sb.append(decodedLine)
+        sb.append(line)
         line = reader.readLine()
     }
 
-    var htmlDocument = Jsoup.parse(sb.toString());
-    val mainDiv = htmlDocument.select("div.clicktable")
-    val entries = mainDiv.select("div.sqdetailsDep.trow")
-
-    entries.forEach{
-        var line = it.select("a span").text().replace("\\s*","")
-        var destination = it.ownText().substringAfter(">>").substringBefore("Gl").replace("\\s+","")
-        var scheduledTime = it.select("span.bold")[1].text().replace("\\s+","")
-        var delay = it.select("span")[1].text().replace("\\s+","")
-
-        println("Line $line to $destination is scheduled for $scheduledTime and currently delayed by $delay minutes.")
-    }
+    return sb.toString()
 }
 
-data class ResponseItem (val time:String,val delay:String, val destination:String)
+data class ResponseItem(val time: String, val delay: String?, val destination: String)
